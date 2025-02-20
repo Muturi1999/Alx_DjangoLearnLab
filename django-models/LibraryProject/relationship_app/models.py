d3bb191bd3e1b53a7e28918f0816616aba9d5e44
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-20 15:23:57 +0300

    library name

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,33 @@
+# relationship_app/models.py
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Libraries"
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    
+    def __str__(self):
+        return f"{self.name} (Librarian at {self.library.name})"
