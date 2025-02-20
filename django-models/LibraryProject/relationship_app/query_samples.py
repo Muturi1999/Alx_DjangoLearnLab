dc3bd570293fdb1e082736d182e22142c506bf10
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-20 11:44:37 +0300

    Fixed query implementations using .get() for correct ORM behavior

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,25 +9,28 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
-    if author:
+    try:
+        author = Author.objects.get(name=author_name)  # Use `.get()`
         books = Book.objects.filter(author=author)
         return [book.title for book in books]
-    return []
+    except Author.DoesNotExist:
+        return []
 
 # List all books in a library
 def books_in_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
+    try:
+        library = Library.objects.get(name=library_name)  # Use `.get()`
         return [book.title for book in library.books.all()]
-    return []
+    except Library.DoesNotExist:
+        return []
 
 # Retrieve the librarian for a library
 def librarian_of_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library and hasattr(library, 'librarian'):
-        return library.librarian.name
-    return None
+    try:
+        library = Library.objects.get(name=library_name)  # Use `.get()`
+        return library.librarian.name if hasattr(library, 'librarian') else None
+    except Library.DoesNotExist:
+        return None
 
 # Sample Usage
 if __name__ == '__main__':
