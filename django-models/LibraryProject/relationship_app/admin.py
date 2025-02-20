899cd3cb26b319fe963f60787e5552a43b4cc1af
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-20 12:09:36 +0300

    Registered models in admin and fixed URL routing

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Book, Library, Librarian
 
-# Register your models here.
+# Register models to appear in Django Admin
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
