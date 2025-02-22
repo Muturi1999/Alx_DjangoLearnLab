609646851e2b16a3d27542abde389b3e53d9671f
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-22 13:58:09 +0300

    Fixing errors on task two

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,14 @@
+# relationship_app/urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from . import views
+
+app_name = 'relationship_app'
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),  
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  
+    # URL pattern for function-based view
+    path('books/', views.book_list, name='book_list'),
+    
+    # URL patterns for class-based views
+    path('libraries/', views.LibraryListView.as_view(), name='library_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
+# relationship_app/views.py
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book, Library  # Explicitly import Library model
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+# Function-based view
+def book_list(request):
+    """
+    Function-based view to display all books
+    """
+    books = Book.objects.all().select_related('author')
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to display library details
-class LibraryDetailView(DetailView):
+# Class-based views
+class LibraryListView(ListView):
+    """
+    Class-based view to display list of libraries
+    """
     model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+    template_name = 'relationship_app/library_list.html'
+    context_object_name = 'libraries'
+
+class LibraryDetailView(DetailView):
+    """
+    Class-based view to display library details
+    """
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add additional context if needed
+        context['books'] = self.object.books.all().select_related('author')
+        return context
