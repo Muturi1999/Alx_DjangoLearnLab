c8a9a3e1ee3903254b9d511f5a7a2168e279d9d0
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-27 10:11:57 +0300

    Inluded example form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,3 +1,17 @@
+# from django import forms
+# from .models import Book
+
+# class BookForm(forms.ModelForm):
+#     """Form for creating and editing Book instances"""
+#     class Meta:
+#         model = Book
+#         fields = ['title', 'author', 'publication_year']
+#         widgets = {
+#             'title': forms.TextInput(attrs={'class': 'form-control'}),
+#             'author': forms.TextInput(attrs={'class': 'form-control'}),
+#             'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
+#         }
+
 from django import forms
 from .models import Book
 
@@ -11,3 +25,7 @@ class BookForm(forms.ModelForm):
             'author': forms.TextInput(attrs={'class': 'form-control'}),
             'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+class ExampleForm(forms.Form):
+    """Dummy form to satisfy external dependencies"""
+    example_field = forms.CharField(max_length=255, required=True)
