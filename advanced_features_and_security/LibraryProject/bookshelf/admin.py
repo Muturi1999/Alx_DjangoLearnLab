fd74c78d4dfc7d51c035b3d4fb4d0e2628263ea4
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-27 08:51:19 +0300

    Fixing admin site registration

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -21,3 +21,6 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'date_of_birth', 'is_staff')
     search_fields = ('username', 'email')
     list_filter = ('is_staff', 'is_superuser')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
