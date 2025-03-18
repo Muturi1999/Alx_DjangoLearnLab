7e8292462720527e427e37097563cbfd294b8c1d
Muturi1999 <mikemuturi1999@gmail.com>
2025-03-18 11:07:56 +0300

    Configured postgress database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -92,12 +92,23 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'Mike_12#$',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
