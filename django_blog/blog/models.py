303ab6964b7e94c1c4835d5b086074c29d4ce811
Muturi1999 <mikemuturi1999@gmail.com>
2025-03-21 20:18:01 +0300

    blog model publish date field

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=now)
+    published_at = models.DateTimeField(default=now)
     updated_at = models.DateTimeField(auto_now=True)
     tags = TaggableManager()
 
