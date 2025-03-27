cbc6330742c7585022d14a71d395af2116f016d1
Muturi1999 <mikemuturi1999@gmail.com>
2025-03-27 10:54:33 +0300

    Added queryset = Comment.objects.all() in CommentViewSet

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,35 @@
+# from rest_framework import viewsets, permissions
+# from .models import Post, Comment
+# from .serializers import PostSerializer, CommentSerializer
+# from django_filters.rest_framework import DjangoFilterBackend
+
+
+
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Post.objects.all().order_by('-created_at')
+#     serializer_class = PostSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['title', 'content']
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+# class CommentViewSet(viewsets.ModelViewSet):
+#     serializer_class = CommentSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def get_queryset(self):
+#         return Comment.objects.filter(post_id=self.kwargs['post_pk']).order_by('-created_at')
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -15,7 +42,9 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all() 
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
@@ -24,5 +53,3 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-
