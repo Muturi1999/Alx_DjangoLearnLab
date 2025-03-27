ff2cd25301a2f556e7da0add617aa7d9265840ee
Muturi1999 <mikemuturi1999@gmail.com>
2025-03-27 09:42:54 +0300

    Updated serializers, added README and requirements.txt

---
+++ social_media_api/README.md
@@ -0,0 +1,4 @@
+# Social Media API
+This project is a Django-based Social Media API built using Django REST Framework (DRF). It includes user authentication, user profiles, posts, comments, likes, and follow functionality.
+
+
--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,3 +1,24 @@
+# from rest_framework import serializers
+# from django.contrib.auth import get_user_model
+# from rest_framework.authtoken.models import Token
+
+# User = get_user_model()
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
+
+# class RegisterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password')
+#         extra_kwargs = {'password': {'write_only': True}}
+
+#     def create(self, validated_data):
+#         user = User.objects.create_user(**validated_data)
+#         Token.objects.create(user=user)
+#         return user
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
@@ -10,6 +31,8 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
