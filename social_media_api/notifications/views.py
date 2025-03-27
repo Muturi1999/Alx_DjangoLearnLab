7b5a594440366cd5b6a83022c7119a5fc246dbf2
Muturi1999 <mikemuturi1999@gmail.com>
2025-03-27 13:27:36 +0300

    timestamp = models.DateTimeField(auto_now_add=True)

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,13 @@
+# from rest_framework import generics, permissions
+# from .models import Notification
+# from .serializers import NotificationSerializer
+
+# class NotificationListView(generics.ListAPIView):
+#     serializer_class = NotificationSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get_queryset(self):
+#         return Notification.objects.filter(recipient=self.request.user).order_by('-created_at')
 from rest_framework import generics, permissions
 from .models import Notification
 from .serializers import NotificationSerializer
@@ -7,4 +17,4 @@ class NotificationListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Notification.objects.filter(recipient=self.request.user).order_by('-created_at')
+        return Notification.objects.filter(recipient=self.request.user).order_by('-timestamp')
